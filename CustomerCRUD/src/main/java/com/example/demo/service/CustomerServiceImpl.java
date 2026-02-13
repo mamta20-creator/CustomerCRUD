863f550f2ab11665425ab3944dbef6628e1e925d
mamta20-creator <mamtachandanshive5@gmail.com>
2026-02-13 19:29:18 +0530

    ythfrt

--- CustomerCRUD/src/main/java/com/example/demo/service/CustomerServiceImpl.java
+++ CustomerCRUD/src/main/java/com/example/demo/service/CustomerServiceImpl.java
@@ -19,66 +19,100 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void add(Customer customer) {
 		// TODO Auto-generated method stub
-		String mob = customer.getMob();
+		//String mob = customer.getMob();
 		
-		if(mob!=null) {
-			mob.trim();
-			if(mob.startsWith("+91")) {
-				mob=mob.substring(3);
+		//if(mob!=null) {
+		//	mob=mob.trim();
+		//	if(mob.startsWith("+91")) {
+		//		mob=mob.substring(3);
 				
-			}
+			//}
 				 
-			if(mob.startsWith("91")) {
-				mob=mob.substring(2);
+			//if(mob.startsWith("91")) {
+			//	mob=mob.substring(2);
 				
-			}
+			//}
 			
 			
-		}
+		//}
        
-        if(mob.length() == 10) {
-			if(mob.charAt(0) == '0' || mob.charAt(0) == '1' || mob.charAt(0) == '2' ||mob.charAt(0) =='3' ||mob.charAt(0) == '4' ||mob.charAt(0) =='5')
-				throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+        //if(mob.length() == 10) {
+			//if(mob.charAt(0) == '0' || mob.charAt(0) == '1' || mob.charAt(0) == '2' ||mob.charAt(0) =='3' ||mob.charAt(0) == '4' ||mob.charAt(0) =='5')
+				//throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
 			
-			for(int i=0; i<mob.length(); i++) {
-				if(!Character.isDigit(mob.charAt(i)))
-					throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+			//for(int i=0; i<mob.length(); i++) {
+				//if(!Character.isDigit(mob.charAt(i)))
+					//throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
 				
-			}
-			List<Customer> list=cr.findAll();
+			//}
+			//List<Customer> list=cr.findAll();
 			
-			for(Customer  c:list) {
-				if(mob.equals(c.getMob())) {
-					throw new InvalidMobileNumber("Mobile Already Exists!!!");
+			//for(Customer  c:list) {
+				//if(mob.equals(c.getMob())) {
+					//throw new InvalidMobileNumber("Mobile Already Exists!!!");
 					
-				}
-			}
-		}else
-			throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+				//}
+			//}
+		//}else
+			//throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
 		
-		customer.setMob(mob);
+		//customer.setMob(mob);
 		 
-        Integer id=customer.getId();
+        //Integer id=customer.getId();
         
-        if(id <= 0) {
-        	throw new InvalidId("Invalid ID");//
-        }
-        List<Customer> list=cr.findAll();
+       // if(id <= 0) {
+        	//throw new InvalidId("Invalid ID");//
+       // }
+       // List<Customer> list=cr.findAll();
 		
-		for(Customer  x : list) {
-			if(id.equals(x.getId())) {
-				throw new InvalidId("Id Already Exists!!!");
+		//for(Customer  x : list) {
+		//	if(id.equals(x.getId())) {
+		//		throw new InvalidId("Id Already Exists!!!");
 				
-			}
+		//	}
 			
 			
+		//}
+        //cr.save(customer);
+		
+		
+
+		    String mob = customer.getMob();
+
+		    if (mob == null) {
+		        throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+		    }
+
+		    mob = mob.trim();
+
+		    if (mob.length() != 10) {
+		        throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+		    }
+
+		    if (mob.charAt(0) == '0' || mob.charAt(0) == '1' || mob.charAt(0) == '2') {
+		        throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+		    }
+
+		    for (int i = 0; i < mob.length(); i++) {
+		        if (!Character.isDigit(mob.charAt(i))) {
+		            throw new InvalidMobileNumber("INVALID MOBILE NUMBER");
+		        }
+		    }
+
+		    for (Customer c : cr.findAll()) {
+		        if (mob.equals(c.getMob())) {
+		            throw new InvalidMobileNumber("Mobile Already Exists");
+		        }
+		    }
+
+		    customer.setMob(mob);
+		    cr.save(customer);
 		}
-        cr.save(customer);
 		
 		
 			
 		
-	}
+	
 
 	@Override
 	public List<Customer> display() {
